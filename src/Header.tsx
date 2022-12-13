07b441457357bda83dedeea31ca63cdb45537cee
Iryna <mulatka781@gmail.com>
2022-12-13 18:50:28 +0200

    css

--- src/Header.tsx
+++ src/Header.tsx
@@ -3,20 +3,20 @@ type Props = {}
 const Header = (props: Props) => {
     return (
         <>
-            <div>
-                <div>
-                    <div>travelaja</div>
-                    <div>
-                        <p>Home</p>
-                        <p>Discover</p>
-                        <p>Services</p>
-                        <p>News</p>
-                        <p>About Us</p>
-                        <p>Contact</p>
+            <div className="head">
+                <div className="navbar">
+                    <div className="logo">travelaja</div>
+                    <div className="menu">
+                        <p className="menu-1">Home</p>
+                        <p className="menu-2">Discover</p>
+                        <p className="menu-3">Services</p>
+                        <p className="menu-4">News</p>
+                        <p className="menu-5">About Us</p>
+                        <p className="menu-6">Contact</p>
                     </div>
                 </div>
-                <div>Explore the world with a smile</div>
-                <p>
+                <div className="title">Explore the world with a smile</div>
+                <p className="text">
                     Lorem ipsum, dolor sit amet consectetur adipisicing elit.
                     Ipsum, repellat. Quidem provident vitae veritatis animi
                     deserunt voluptatem, rerum officia ad ullam natus enim,
