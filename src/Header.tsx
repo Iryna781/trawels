eaba992d59c305b7a3d0ece4084d98d46382f829
Iryna <mulatka781@gmail.com>
2022-12-13 16:12:11 +0200

    navbar

---
+++ src/Header.tsx
@@ -0,0 +1,30 @@
+import './Header.css'
+type Props = {}
+const Header = (props: Props) => {
+    return (
+        <>
+            <div>
+                <div>
+                    <div>travelaja</div>
+                    <div>
+                        <p>Home</p>
+                        <p>Discover</p>
+                        <p>Services</p>
+                        <p>News</p>
+                        <p>About Us</p>
+                        <p>Contact</p>
+                    </div>
+                </div>
+                <div>Explore the world with a smile</div>
+                <p>
+                    Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                    Ipsum, repellat. Quidem provident vitae veritatis animi
+                    deserunt voluptatem, rerum officia ad ullam natus enim,
+                    pariatur excepturi laudantium praesentium deleniti
+                    exercitationem itaque.
+                </p>
+            </div>
+        </>
+    )
+}
+export default Header
--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import Header from './Header'
+
+const App = () => {
+    return (
+        <>
+            <Header />
+        </>
+    )
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
-root.render(<React.StrictMode></React.StrictMode>)
+root.render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
+)
