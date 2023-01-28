037bd1168d8537b8986253ed28f9007a1e2c0f82
Iryna <mulatka781@gmail.com>
2023-01-28 09:16:07 +0200

    discover

--- src/components/Discovertext/Discovertexteurope.tsx
+++ src/components/Discovertext/Discovertexteurope.tsx
@@ -1,5 +1,84 @@
+import Sliderdiscovereurope from '../Slider/Sliderdiscovereurope'
+
 type Props = {}
 const Discovertexteurope = (props: Props) => {
-    return <div>Discovertexteurope</div>
+    return (
+        <>
+            <div className="first">
+                <div className="first-slider">
+                    <Sliderdiscovereurope />
+                </div>
+                <div className="first-text">
+                    <p className="title-text">France</p>
+                    <p className="text-1">
+                        France has fans all over the world due to its tasty
+                        Bordeaux wines, great cheese, the Eiffel tower, severe
+                        Normandy castles and world-class fashion. Its beautiful
+                        Riviera beaches, whine valleys, deep forests, historic
+                        towns and ancient chateaus. There is nothing like
+                        drinking a bottle of wine with fresh croissant on Seine
+                        river bank or taking a day trip to the French
+                        countryside. France has so much to see and do, and will
+                        never leave your disappointed.
+                    </p>
+                    <p className="title-text">Finland </p>
+                    <p className="text-1">
+                        Finland is the land of Santa Claus, Northern Lights,
+                        mountains, waterfalls and all kind of different
+                        sceneries! The place is perfect for the outdoors
+                        activities lovers such as skiing, reindeer riding, husky
+                        ride or Northern lights hunding on a snowmobile. Finland
+                        is perfect in every season, however the best time to
+                        explore this great destination is winter. Keep in mind
+                        that it can be extremely cold, so bring winter clothes
+                        with you. With our great travel guides, we will help you
+                        plan perfect Finnish tours and get the most of your
+                        stay.
+                    </p>
+                </div>
+            </div>
+            <div className="text-third">
+                <p className="title-text">Italy </p>
+                <p className="text-1">
+                    Amazing food, romantic countryside and wine that can't be
+                    found anywhere else on the planet, make Italy a top
+                    destination to add in your bucket list. You will fall in
+                    love with more and more with every visit. Tuscany vineyards,
+                    Florentine architecture, the Imperial Rome, or the laid back
+                    nature of Amalfi Coast and breathtaking Venice canals, all
+                    make it a must-visit place. With so much to see and explore,
+                    it will take ages to find out what really Italy is from the
+                    first sight. But don't be overhelmed by this, there's still
+                    some great places to visit during your two-week vacation or
+                    few months long trip.
+                </p>
+                <p className="title-text"> Spain</p>
+                <p className="text-1">
+                    Spain is a true land of siesta which affects the daily
+                    routine of locals and tourists from all over the world where
+                    everything runs slowly and staying up late. No one cares
+                    about anything and people just enjoy life there, that is why
+                    tours to Spain is a pure fun! Spain is a lively, bustling
+                    country, a true melting pot of different cultures and
+                    nations. Madrid and Barcelona lively capital cities, Granada
+                    has an Arabic heritage hidden in every corner, Basque
+                    Country in the Northern Spain is a unique place with its
+                    local traditions that differ a lot from the rest of the
+                    country. And,moreover, Spain is extremely affordable to
+                    visit!
+                </p>
+                <p className="title-text">Norway</p>
+                <p className="text-1">
+                    The land of Vikings and amazing fjords like nowhere else,
+                    Norway is so amazingly beautiful that you should definitely
+                    include into your travel bucket list. The Norwegians are
+                    wonderful and welcoming people; everyone speaks fluent
+                    English, and the nature attractions should not be ignored in
+                    any case. There is nothing like sailing around the fjords in
+                    the north onboard of a Hurtigruten ship.
+                </p>
+            </div>
+        </>
+    )
 }
 export default Discovertexteurope
--- src/components/Slider/Sliderdiscovereurope.tsx
+++ src/components/Slider/Sliderdiscovereurope.tsx
@@ -1,5 +1,75 @@
+import { Swiper, SwiperSlide } from 'swiper/react'
+import { Pagination } from 'swiper'
+import 'swiper/css'
+import 'swiper/css/pagination'
 type Props = {}
 const Sliderdiscovereurope = (props: Props) => {
-    return <div>Sliderdiscovereurope</div>
+    return (
+        <>
+            <Swiper
+                direction={'vertical'}
+                pagination={{
+                    clickable: true,
+                }}
+                modules={[Pagination]}
+                className="mySwiper"
+            >
+                <SwiperSlide className="slid">
+                    <img
+                        src="https://turizm.world/wp-content/uploads/2018/07/dostoprimechatelnosti-francii.jpg"
+                        alt=""
+                    />
+                </SwiperSlide>
+                <SwiperSlide className="slid">
+                    <img
+                        src="https://touristam.com/wp-content/uploads/2019/01/norvegiya-7.jpg"
+                        alt=""
+                    />
+                </SwiperSlide>
+                <SwiperSlide className="slid">
+                    <img
+                        src="https://www.tripzaza.com/ru/destinations/wp-content/uploads/2018/02/18-The_Northern_lights-e1518242269960.jpg"
+                        alt=""
+                    />
+                </SwiperSlide>
+                <SwiperSlide className="slid">
+                    <img
+                        src="https://www.tripzaza.com/ru/destinations/wp-content/uploads/2017/04/Italy-9-The-Grand-Canal-in-Venice-e1492908547833.jpg"
+                        alt=""
+                    />
+                </SwiperSlide>
+                <SwiperSlide className="slid">
+                    <img
+                        src="https://turvopros.com/wp-content/uploads/2016/04/Batlo.jpg"
+                        alt=""
+                    />
+                </SwiperSlide>
+                <SwiperSlide className="slid">
+                    <img
+                        src="https://34travel.me/media/posts/5817092c50c73-1.jpg"
+                        alt=""
+                    />
+                </SwiperSlide>
+                <SwiperSlide className="slid">
+                    <img
+                        src="https://e-finland.ru/media/images/stories/images/stories/img_9/parvoo_big.jpg"
+                        alt=""
+                    />
+                </SwiperSlide>
+                <SwiperSlide className="slid">
+                    <img
+                        src="https://e-finland.ru/media/images/stories/images/stories/img_9/parikalla_big.jpg"
+                        alt=""
+                    />
+                </SwiperSlide>
+                <SwiperSlide className="slid">
+                    <img
+                        src="https://media-1obl-ru.storage.yandexcloud.net/resize_cache/817886/e71ad92e8a086fd0ad26a63a20c0e003/iblock/c3a/c3a75250909f561217c3fbb2c6933717.jpg"
+                        alt=""
+                    />
+                </SwiperSlide>
+            </Swiper>
+        </>
+    )
 }
 export default Sliderdiscovereurope
